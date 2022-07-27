8dbc81d8b01cff1f1887582b4e85641382a452de
DanielVII <danieldantaswa@gmail.com>
2022-07-27 19:35:14 -0300

    Criando os viesets para trabalhar com as requisições por meio da API

--- Django/005-Django-Rest-Framework/setup/setup/urls.py
+++ Django/005-Django-Rest-Framework/setup/setup/urls.py
@@ -1,8 +1,13 @@
+from rest_framework import routers
+from django.urls import path, include
+from escola.views import AlunosViewSet, CursosViewSet
 from django.contrib import admin
-from django.urls import path
-from escola.views import alunos
+
+router = routers.DefaultRouter()
+router.register('alunos', AlunosViewSet, basename='Alunos')
+router.register('cursos', CursosViewSet, basename='Cursos')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('alunos/', alunos),
+    path('', include(router.urls)),
 ]
