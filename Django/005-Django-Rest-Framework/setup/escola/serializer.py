f507dbb2b339ebae06ee64f75b659abbf0398645
DanielVII <danieldantaswa@gmail.com>
2022-07-31 11:12:35 -0300

    Criando tabela e viewset para Matriculas

--- Django/005-Django-Rest-Framework/setup/escola/serializer.py
+++ Django/005-Django-Rest-Framework/setup/escola/serializer.py
@@ -1,13 +1,18 @@
 from dataclasses import field
 from rest_framework import serializers
-from escola.models import Aluno, Curso
+from escola.models import Aluno, Curso, Matricula
 
 class AlunoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Aluno
         fields = '__all__'
 
-class CursoSerializer(serializers.ModelField):
+class CursoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Curso
         fields = '__all__'
+
+class MatriculoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Matricula
+        exclude = []
